2c29158592933f70e942f4ca6b86ca49f1fb7d18
Luis Toscano <commentariosweb@gmail.com>
2025-02-24 17:58:20 -0500

    Update routes.helper.ts

--- src/helpers/routes.helper.ts
+++ src/helpers/routes.helper.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 export function setRoutes(Router:any, Routes:Array<any>) {
-    const RouterStore = express.Router();
+    const RouterStore:any = express.Router();
     
     Routes.map((route:any) => {
         RouterStore[route.method](route.path, route.controller);
